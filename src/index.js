304ab564317a481863c15fe151026e2a567d908a
Daichi Nishida <verhichi@hotmail.com>
2018-11-05 22:36:26 +0900

    Change code to import "App" component and render "App" Component.
    -

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './app/app';
+
 import './normalize.css';
 
 ReactDOM.render(
-     <h1>YEAH</h1>
+     <App/>
     ,document.getElementById('root')
 )
