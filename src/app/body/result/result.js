4ba4bc67402cc3c0c12c94596dc280d78c7650b2
Daichi Nishida <verhichi@hotmail.com>
2018-11-05 23:07:52 +0900

    Create "Result" Component.
    -

---
+++ src/app/body/result/result.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class Result extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <div>Result</div>
+    );
+  }
+}
