b1b9af9a11b1351a2253b12523d0eb7316efccd6
Daichi Nishida <verhichi@hotmail.com>
2018-11-05 22:42:55 +0900

    Create "App" component that imports and renders the "Header", "Body", "Footer" components.
    -

---
+++ src/app/app.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+import Header from './header/header';
+import Body from './body/body';
+import Footer from './footer/footer';
+
+export default class PageBody extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <div>
+        <Header/>
+        <Body/>
+        <Footer/>
+      </div>
+    );
+  }
+}
