254aa7bb65018747567ba8e4a975dc910fd892df
Daichi Nishida <verhichi@hotmail.com>
2018-11-06 18:38:14 +0900

    Add placeholder JSX for result table.
    This table will list all the results found through the search.

--- src/app/body/result/result.js
+++ src/app/body/result/result.js
@@ -8,7 +8,41 @@ export default class Result extends Component{
   render(){
     return (
       <div className="result-table-container">
-        <h1>Found: 8 result(s)</h1>
+        <h1>Found: 5 result(s)</h1>
+        <table className="result-table">
+          <thead>
+            <th>Library</th>
+            <th>Title</th>
+            <th>Artist</th>
+          </thead>
+          <tbody>
+            <tr>
+              <td>TITLE1</td>
+              <td><a href="#" target="_blank">Link1</a></td>
+              <td>ARTIST NAME1</td>
+            </tr>
+            <tr>
+              <td>TITLE2</td>
+              <td><a href="#" target="_blank">Link2</a></td>
+              <td>ARTIST NAME2</td>
+            </tr>
+            <tr>
+              <td>TITLE3</td>
+              <td><a href="#" target="_blank">Link3</a></td>
+              <td>ARTIST NAME3</td>
+            </tr>
+            <tr>
+              <td>TITLE4</td>
+              <td><a href="#" target="_blank">Link4</a></td>
+              <td>ARTIST NAME4</td>
+            </tr>
+            <tr>
+              <td>TITLE5</td>
+              <td><a href="#" target="_blank">Link5</a></td>
+              <td>ARTIST NAME5</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
