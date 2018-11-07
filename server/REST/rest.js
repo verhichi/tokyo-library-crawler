9ea47244260f562c24c250b595af369f3f5cc380
Daichi Nishida <verhichi@hotmail.com>
2018-11-07 21:17:41 +0900

    Add code for "/rest/crawl".
    -

--- server/REST/rest.js
+++ server/REST/rest.js
@@ -39,8 +39,51 @@ router.get('/search_type', (req, res) => {
 // GET search result from scraping the library websites
 router.get('/crawl', (req, res) => {
   console.log('Request GET /rest/crawl');
-  const result = [{library: "lib1", title: "title1", link: "www.google.com", artist: "artist1"}, {library: "lib2", title: "title2", link: "www.yahoo.com", artist: "artist2"}];
-  res.json({result});
+
+  console.log('Searching for keyword:', req.query.search_option.keyword);
+
+  const search_library_obj_array = library_json.filter((library) => req.query.search_option.checked_library.includes(library.key));
+
+  const promise_array = search_library_obj_array.map((library) => {
+    return cheerio.fetch(library.url)
+      .then((page) => {
+        const $ = page.$;
+
+        let search_option = library.fields;
+
+        search_option[library.keywordName] = req.query.search_option.keyword;
+        search_option[library.searchTypeEle] = req.query.search_options.search_type == '0' ? library.searchByTitle : library.searchByArtist;
+
+        $(library.formEle).field(search_option);
+
+        return $(library.submitButEle).click();
+      })
+      .then((page) => {
+        const $ = page.$;
+
+        const url_array    = $(library.listUrlEle).toArray().map(ele =>    $(ele).url()  );
+        const title_array  = $(library.listTitleEle).toArray().map(ele =>  $(ele).text() );
+        const artist_array = $(library.listArtistEle).toArray().map(ele => $(ele).text() );
+
+        const media_array = url_array.map((url, idx) => {
+          return {
+            library: library.name_en,
+            title:   title_array[idx],
+            artist:  artist_array[idx],
+            link:    url
+          };
+        });
+
+        resolve(media_array);
+      });
+  });
+
+  Promise.all(promise_array).then((result_array) => {
+    res.json({result: [].concat(...result_array)});
+  });
+
+  // const result = [{library: "lib1", title: "title1", link: "www.google.com", artist: "artist1"}, {library: "lib2", title: "title2", link: "www.yahoo.com", artist: "artist2"}];
+  // res.json({result});
 });
 
 
