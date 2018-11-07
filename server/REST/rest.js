e5f1ba88155085b00e12a14e2c53a917b9bb9a99
Daichi Nishida <verhichi@hotmail.com>
2018-11-07 15:18:45 +0900

    Add code to import "express" and "express.Router". Add code to import "library.json" and "search_type.json". Add relative comments.
    -

---
+++ server/REST/rest.js
@@ -0,0 +1,17 @@
+/******************************************************************************
+ *                                 Express Setting
+ ******************************************************************************/
+const express = require('express'); // import express framework
+const router = express.Router();    // create routing object
+
+
+/******************************************************************************
+ *                                 Data Setting
+ ******************************************************************************/
+const library_json =     require('../data/library.json');     // json file of library information
+const search_type_json = require('../data/search_type.json'); // json file of search_type
+
+
+/******************************************************************************
+ *                                 Routes
+ ******************************************************************************/
