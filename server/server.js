1a54f9b4549f96809c1c7a63eef2134a5192a6d7
Daichi Nishida <verhichi@hotmail.com>
2018-11-07 14:19:37 +0900

    Add code to import and use body-parser to parse body to json. Add code to import and use "rest.js" file for rest uri's.
    -

--- server/server.js
+++ server/server.js
@@ -9,6 +9,10 @@ const express = require('express') // import express framework
 const app = express();             // instantiate express
 const port = 3000;                 // set default port number
 
+const body_parser = require('body_parser');        // import body-parser
+app.use(body_parser.urlencoded({extended: true})); // use body-parser
+app.use(body_parser.json());                       // parse body to json
+
 // set express listener
 var listener = app.listen(port, () => {
   const server_start_timestamp = new Date().toLocaleString();
@@ -24,11 +28,14 @@ var listener = app.listen(port, () => {
 /******************************************************************************
  *                               Path Setup
  ******************************************************************************/
-const path = require('path'); // import path object
+const rest_routes = require('./REST/rest.js'); // import REST route file
+const path = require('path');                  // import path object
 
 app.use(express.static(path.join(__dirname, '../build'))); // Set path for file to ../build
 
-// set all URL to open index.html
+app.use('/', rest_routes); // REST routing
+
+// Set Unknown URL to open index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build/index.html'));
 });
