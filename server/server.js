950ef58badfca3b4dc04deac7d8c71285e874146
Daichi Nishida <verhichi@hotmail.com>
2018-11-08 02:56:49 +0900

    Add code to import "cluster" so that multi-core processing is available.
    -

--- server/server.js
+++ server/server.js
@@ -2,40 +2,64 @@
 // The express framework will setup on this file.
 // All other server-side logic and tools will branch from this file.
 
+
 /******************************************************************************
  *                                 Server Setup
  ******************************************************************************/
-const express = require('express') // import express framework
-const app = express();             // instantiate express
-const port = 3000;                 // set default port number
+const cluster = require('cluster');
 
-const body_parser = require('body-parser');        // import body-parser
-app.use(body_parser.urlencoded({extended: true})); // use body-parser
-app.use(body_parser.json());                       // parse body to json
+// Code to run if we're in the master process
+if(cluster.isMaster){
 
-// set express listener
-var listener = app.listen(port, () => {
-  const server_start_timestamp = new Date().toLocaleString();
+  const master_start_timestamp = new Date().toLocaleString();
+  let cpu_count = require('os').cpus().length;
 
-  console.log('\n--- SERVER STATUS ---');
-  console.log('Server Start Date:', server_start_timestamp);
+  console.log('\n--- MASTER CLUSTER STATUS ---');
+  console.log(master_start_timestamp);
+  console.log('Master Cluster launched successfully');
+  console.log(`There are ${cpu_count} cpu(s) on this machine`);
+  console.log('-----------------------------\n');
 
-  console.log('The Server is up and running!')
-  console.log(`Access via: http://localhost:${port}`)
-});
+  for(let idx = 0; idx < cpu_count; idx++){
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.id} has died...creating a new one`);
+    cluster.fork();
+  });
+
+} else {
+
+  const express = require('express') // import express framework
+  const app = express();             // instantiate express
+  const port = 3000;                 // set default port number
+
+  const body_parser = require('body-parser');        // import body-parser
+  app.use(body_parser.urlencoded({extended: true})); // use body-parser
+  app.use(body_parser.json());                       // parse body to json
+
+  // set express listener
+  var listener = app.listen(port, () => {
+    const server_start_timestamp = new Date().toLocaleString();
+
+    console.log(`[${server_start_timestamp}] Worker ID: ${cluster.worker.id} - Worker loaded up successfully!`);
+  });
 
 
-/******************************************************************************
- *                               Path Setup
- ******************************************************************************/
-const rest_routes = require('./REST/rest'); // import REST route file
-const path = require('path');                  // import path object
+  /******************************************************************************
+  *                               Path Setup
+  ******************************************************************************/
+  const rest_routes = require('./REST/rest'); // import REST route file
+  const path = require('path');                  // import path object
 
-app.use(express.static(path.join(__dirname, '../build'))); // Set path for file to ../build
+  app.use(express.static(path.join(__dirname, '../build'))); // Set path for file to ../build
 
-app.use('/rest', rest_routes); // REST routing
+  app.use('/rest', rest_routes); // REST routing
 
-// Set Unknown URL to open index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build/index.html'));
-});
+  // Set Unknown URL to open index.html
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+  });
+
+}
