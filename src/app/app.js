e2a729485b67124f2c40ae4df719364c3158f167
Daichi Nishida <verhichi@hotmail.com>
2018-11-08 20:19:48 +0900

    Add code to import "Help" component. Add state "show_help" in "app.js". Add method "showHelp" and "hideHelp" that changes the boolean state of "show_help" and bind with class in constructor. Set "showHelp" method as "Header" component's property. Set "hideHelp" method and "show_help" state as "Help" component's property.
    -

--- src/app/app.js
+++ src/app/app.js
@@ -4,20 +4,35 @@ import Header from './header/header';
 import Body from './body/body';
 import Footer from './footer/footer';
 import Error from './error/error';
+import Help from './help/help';
 
 export default class App extends Component{
   constructor(props){
     super(props);
+    this.state = {
+      show_help: false;
+    };
+    this.showHelp = this.showHelp.bind(this);
+    this.hideHelp = this.hideHelp.bind(this);
+  }
+
+  showHelp(){
+    this.setState({show_help: true});
+  }
+
+  hideHelp(){
+    this.setState({show_help: false});
   }
 
   render(){
     return (
       <Fragment>
-        <Header/>
+        <Header showHelp={this.state.showHelp}/>
         <Error>
           <Body/>
         </Error>
         <Footer/>
+        <Help show_help={this.state.show_help} hideHelp={this.state.hideHelp}/>
       </Fragment>
     );
   }
