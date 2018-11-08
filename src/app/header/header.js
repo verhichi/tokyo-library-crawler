8dc82d8c107d09262ef3150387ffa4d3f82b4a8c
Daichi Nishida <verhichi@hotmail.com>
2018-11-08 20:55:08 +0900

    Change JSX of "header.js" to be <div> instead of <h1>. Change styling of header.
    -

--- src/app/header/header.js
+++ src/app/header/header.js
@@ -8,8 +8,8 @@ export default class Header extends Component{
   render(){
     return (
       <header>
-        <h1>Tokyo Library Audio Crawler</h1>
-        <h1 className="show-help" onClick={this.props.showHelp}>HELP</h1>
+        <div>Tokyo Library Audio Crawler</div>
+        <div className="show-help" onClick={this.props.showHelp}>HELP</div>
       </header>
     );
   }
