b5276a87dd5ab0c12a010bb82d7d93df35280449
Daichi Nishida <verhichi@hotmail.com>
2018-11-09 20:32:44 +0900

    Changed JSX of "help.js" to show a more detailed help text. Add styling to help text.
    -

--- src/app/help/help.js
+++ src/app/help/help.js
@@ -13,9 +13,51 @@ export default class Help extends Component{
           <div className="help-container">
             <div className="close-help" onClick={this.props.hideHelp}>CLOSE</div>
             <div className="help-content">
-              <h1>What is this?</h1>
-              <h4>This is a web crawling application that searches libraries in Tokyo for audio media(CDs). At this moment, only a single instance of the web crawling logic can be run at a time. So whenever someone is using it, you will not be able to search until he/she is done with their search.</h4>
-              <h4>The front-end is made with React.js and the back-end is made with node.js(Express).</h4>
+              <div className="help-main-title">HELP</div>
+              <div className="help-section-container">
+                <div className="help-section-title">What even is this?</div>
+                <div className="help-section-text">This is a web application that searches libraries in Tokyo for audio media(CD).</div>
+              </div>
+              <div className="help-section-container">
+                <div className="help-section-title">How do I use it?</div>
+                <div className="help-section-text">
+                  <ol>
+                    <li>Input keyword that you want to search with.</li>
+                    <li>Input whether you want to search the keyword as the title or the aritst.</li>
+                    <li>Check which wards you want to search in.</li>
+                    <li>Click the Search button and wait.</li>
+                    <li>???</li>
+                    <li>PROFIT!</li>
+                  </ol>
+                </div>
+              </div>
+              <div className="help-section-container">
+                <div className="help-section-title">Warnings</div>
+                <div className="help-section-text">
+                  <ul>
+                    <li>Only a single instance can be run at a time; in other words, if another user is in the middle of his/her search, you will have to wait for his/her search to end before you can start your own.</li>
+                    <li>Each library gets 5 minutes before it times out.</li>
+                    <li>This website is designed only for PC.</li>
+                    <li>This website is designed to be used by chrome, firefox, safari, and edge(NOT INTERNET EXPLORER).</li>
+                  </ul>
+                </div>
+              </div>
+              <div className="help-section-container">
+                <div className="help-section-title">What did you use to make this?</div>
+                <div className="help-section-text">The front-end is made with React.js and the back-end is made with node.js(express).</div>
+              </div>
+              <div className="help-section-container">
+                <div className="help-section-title">How long did this take to make?</div>
+                <div className="help-section-text">About a week? If you want to see the process, check out my github.</div>
+              </div>
+              <div className="help-section-container">
+                <div className="help-section-title">Why did you make this?</div>
+                <div className="help-section-text">I was personally asked by a friend if I could somehow facilitate his library expeditions to find audio media. So I made this for him.</div>
+              </div>
+              <div className="help-section-container">
+                <div className="help-section-title">Any last words?</div>
+                <div className="help-section-text">I understand that there is a lot of room for improvement(and features that need to be fixed) but because this is just a personal project that I made for fun, I'm not gonna take my time to fix it(unless requested by my friend). So if there is a problem...just deal with it.</div>
+              </div>
             </div>
           </div>
         </div>
