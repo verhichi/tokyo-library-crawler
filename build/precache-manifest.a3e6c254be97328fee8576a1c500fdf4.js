f568d3a717a9a24f4ee6533d1057ec9503333279
Daichi Nishida <verhichi@hotmail.com>
2018-11-12 05:04:14 +0900

    Change ".gitignore" to not ignore "build" file for heroku.
    -

---
+++ build/precache-manifest.a3e6c254be97328fee8576a1c500fdf4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccc6f0d09e86b469efaf1752510bb996",
+    "url": "/static/media/book.ccc6f0d0.jpg"
+  },
+  {
+    "revision": "6df5ccbaea64b33136998b793f029293",
+    "url": "/static/media/ColabThi-webfont.6df5ccba.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b31b315b86ad2bf7b3b5",
+    "url": "/static/js/main.b31b315b.chunk.js"
+  },
+  {
+    "revision": "12af20dfe47b9b052258",
+    "url": "/static/js/1.12af20df.chunk.js"
+  },
+  {
+    "revision": "b31b315b86ad2bf7b3b5",
+    "url": "/static/css/main.19440e90.chunk.css"
+  },
+  {
+    "revision": "fa639524a6c3e72602937420fd436a8a",
+    "url": "/index.html"
+  }
+];
